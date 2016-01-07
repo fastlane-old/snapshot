e3c148ed60eef38ca276eaf46eb975b3cd34344e
Maxime Britto <maxime.britto@gmail.com>
2016-01-07 11:26:44 +0100

    Alert amangement
    - Adds a function to allow automatic dismissal of alerts that prevent
    actions or snapshots
    - Adds the possibility to wait for any alert to be dismissed before a
    snapshot is taken (disabled by default)

---
+++ lib/assets/SnapshotAlertManager.swift
@@ -0,0 +1,60 @@
+//
+//  SnapshotAlertManager.swift
+//
+//  Created by Maxime Britto on 06/01/2016.
+//  Copyright © 2016 dev2a. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+var alertInterruptionMonitor:NSObjectProtocol?
+
+func automaticallyDismissAlerts(testCase:XCTestCase) -> NSObjectProtocol
+{
+    return SnapshotAlertManager.automaticallyDismissAlerts(testCase)
+}
+
+func stopDismissingAlerts(testCase:XCTestCase) {
+    SnapshotAlertManager.stopDismissingAlerts(testCase)
+}
+
+func waitForAlertsToBeDismissed() {
+    SnapshotAlertManager.waitForAlertsToBeDismissed()
+}
+
+class SnapshotAlertManager: NSObject {
+    class func stopDismissingAlerts(testCase:XCTestCase) {
+        if let previousDismissHandler = alertInterruptionMonitor {
+            testCase.removeUIInterruptionMonitor(previousDismissHandler)
+        }
+    }
+
+    class func automaticallyDismissAlerts(testCase:XCTestCase) -> NSObjectProtocol
+    {
+        SnapshotAlertManager.stopDismissingAlerts(testCase)
+        alertInterruptionMonitor = testCase.addUIInterruptionMonitorWithDescription("Snapshot alert auto hide") { (alert: XCUIElement) -> Bool in
+            print("Alert \"\(alert.label)\" will be dismissed")
+            return false
+        }
+
+        return alertInterruptionMonitor!
+    }
+
+    class func waitForAlertsToBeDismissed()
+    {
+        let query = XCUIApplication().alerts
+        var waitCounter = 0
+        while (query.count > 0) {
+            if alertInterruptionMonitor == nil {
+                print("The SnapshotAlertManager can automatically hide alerts that will prevent action or screenshots. Just call this method to enable it : automaticallyDismissAlerts(testCase:XCTestCase)")
+            }
+            sleep(1)
+            waitCounter++
+            print("Found an alert... waiting for it to disappear")
+            if waitCounter > 2 {
+                XCUIApplication().tap() //UIInterruptionMonitor will only trigger if an action is prevented by the alert so we generate one if the user script hasn't after 2 seconds
+            }
+        }
+    }
+}
